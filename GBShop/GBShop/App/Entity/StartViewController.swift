a9b522f4fadf145c73fe7935c56f2c0b198e5a52
Станислав <kaopuk@gmail.com>
2020-12-17 21:03:27 +0300

    first home work

---
+++ GBShop/GBShop/App/Entity/StartViewController.swift
@@ -0,0 +1,18 @@
+//
+//  StartViewController.swift
+//  GBShop
+//
+//  Created by Stanislav Slipchenko on 17.12.2020.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+    }
+    
+}
