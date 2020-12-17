4e29194b514503fdc972335a99c6391c13e637ba
slipStas <41761920+slipStas@users.noreply.github.com>
2020-12-17 20:09:30 +0300

    init

---
+++ GBShop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GBShop
+//
+//  Created by Stanislav Slipchenko on 17.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
